5103349164bb18aa929bde270cf8f134692fe84d
skicodes <skalrashed02@hotmail.com>
2025-08-25 09:37:22 +0000

    dkdk

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,10 +3,12 @@
 /**
  * main - Entry point
  *
+ * Description: A C program that prints with puts function
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
