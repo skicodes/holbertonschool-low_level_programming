7383ba09857adbbbe655a7e54b96a73ed77056f1
skicodes <skalrashed02@hotmail.com>
2025-08-26 11:49:31 +0000

    sksks

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: The integer to compute
+ *
+ * Return: The absolute value of n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	return (n);
+}
