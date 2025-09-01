31002db720a4edfe66c9554cfd1a3860e53d1c2a
skicodes <skalrashed02@hotmail.com>
2025-09-01 09:59:14 +0000

    dkdjd

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,6 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * File: main.h
+ * Author: Your Name
+ * Description: Header file containing prototypes for all functions
+ *              used in the 0x04-more_functions_nested_loops project.
+ */
+
 int _putchar(char c);
 int _isupper(int c);
 
