d16f8614f0ed0f75878ee528b2920182e8e268d7
skicodes <skalrashed02@hotmail.com>
2025-09-01 11:08:41 +0000

    jfjfj;ojdlljisj

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers from 0 to 9, except 2 and 4
+ *
+ * You can only use _putchar twice in the code.
+ */
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		if (c != '2' && c != '4')
+			_putchar(c);
+	}
+	_putchar('\n');
+}
