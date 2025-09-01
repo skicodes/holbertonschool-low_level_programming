8c98c6b4df9d23949eaa9e63c83bd58162039ec8
skicodes <skalrashed02@hotmail.com>
2025-09-01 10:41:52 +0000

    kkdkfkff

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -2,15 +2,14 @@
 
 /**
  * _isupper - checks for uppercase character
- * @c: the character to check
+ * @c: The character to check
  *
  * Return: 1 if c is uppercase, 0 otherwise
  */
 int _isupper(int c)
 {
-    if (c >= 'A' && c <= 'Z')
-    {
-        return (1);
-    }
-    return (0);
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
 }
