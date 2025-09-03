d9f93480253911469fec28aeb3f1b1753222f6f8
skicodes <skalrashed02@hotmail.com>
2025-09-03 12:37:43 +0000

    jdhufj

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -13,18 +13,26 @@ int _atoi(char *s)
 
 	while (s[i] != '\0')
 	{
-		/* handle signs before the number */
 		if (s[i] == '-' && !started)
 			sign *= -1;
 		else if (s[i] == '+' && !started)
 			;
-		/* if digit found */
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
 			started = 1;
+
+			/* Build the number carefully */
+			if (result > (2147483647 - (s[i] - '0')) / 10)
+			{
+				/* handle overflow for INT_MAX/INT_MIN */
+				if (sign == 1)
+					return (2147483647);
+				else
+					return (-2147483648);
+			}
+
 			result = result * 10 + (s[i] - '0');
 		}
-		/* stop when digits end */
 		else if (started)
 			break;
 
