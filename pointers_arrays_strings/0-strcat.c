43a0aa3f2c9c24028a6ab9f270c9685cff70035e
skicodes <skalrashed02@hotmail.com>
2025-09-03 12:47:50 +0000

    jhdhh

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -3,27 +3,27 @@
 /**
  * _strcat - concatenates the string pointed to by src
  *           to the end of the string pointed to by dest
- * @dest: string to which src will be appended
- * @src: string to append to dest
+ * @dest: pointer to the destination string
+ * @src: pointer to the source string
  *
  * Return: pointer to dest
  */
 char *_strcat(char *dest, char *src)
 {
-    int i = 0;
-    int j = 0;
+	int i = 0;
+	int j = 0;
 
-    while (dest[i] != '\0')
-        i++;
+	while (dest[i] != '\0')
+		i++;
 
-    while (src[j] != '\0')
-    {
-        dest[i] = src[j];
-        i++;
-        j++;
-    }
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 
-    dest[i] = '\0';
+	dest[i] = '\0';
 
-    return (dest);
+	return (dest);
 }
