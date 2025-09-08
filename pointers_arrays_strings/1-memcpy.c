46ae8e6aa32c76eec90440ddaac6f234c8ac426f
skicodes <skalrashed02@hotmail.com>
2025-09-08 19:40:17 +0000

    edit

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -2,20 +2,22 @@
 
 /**
  * _memcpy - copies memory area
- * @dest: pointer to destination memory
- * @src: pointer to source memory
+ * @dest: pointer to the destination memory
+ * @src: pointer to the source memory
  * @n: number of bytes to copy
  *
  * Return: pointer to dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-    unsigned int i;
+	unsigned int i;
 
-    for (i = 0; i < n; i++)
-    {
-        dest[i] = src[i];
-    }
+	i = 0;
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
 
-    return (dest);
+	return (dest);
 }
