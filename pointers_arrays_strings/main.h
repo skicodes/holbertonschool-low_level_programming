120a6cbe20f48607d11d5ad44135201812598bd6
skicodes <skalrashed02@hotmail.com>
2025-09-08 19:38:06 +0000

    loll

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -22,5 +22,6 @@ char *cap_string(char *str);
 char *leet(char *str);
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif
