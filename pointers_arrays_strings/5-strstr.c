e28841b99a3b1d68b3d6090b672a25e6c596aea7
skicodes <skalrashed02@hotmail.com>
2025-09-09 14:42:21 +0000

    mhm

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: the string to search in
+ * @needle: the substring to search for
+ *
+ * Return: a pointer to the beginning of the located substring,
+ * or NULL if the substring is not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *h, *n;
+
+	if (*needle == '\0')
+		return (haystack);
+
+	while (*haystack != '\0')
+	{
+		h = haystack;
+		n = needle;
+
+		while (*h != '\0' && *n != '\0' && *h == *n)
+		{
+			h++;
+			n++;
+		}
+
+		if (*n == '\0')
+			return (haystack);
+
+		haystack++;
+	}
+
+	return (0);
+}
