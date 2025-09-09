b27d9ed367e5e4da5f505ee64ce415f211834502
skicodes <skalrashed02@hotmail.com>
2025-09-09 13:49:57 +0000

    dhdhdh

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -5,19 +5,19 @@
  * @s: the string to search
  * @c: the character to find
  *
- * Return: pointer to the first occurrence of c, or NULL if not found
+ * Return: pointer to first occurrence of c in s, or NULL if not found
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
-	{
-		if (*s == c)
-			return (s);
-		s++;
-	}
+    while (*s != '\0')
+    {
+        if (*s == c)
+            return (s);
+        s++;
+    }
 
-	if (c == '\0')
-		return (s);
+    if (c == '\0') /* handle case where c is '\0' */
+        return (s);
 
-	return (NULL);
+    return (0);
 }
