4982a4a5122100901edc23b27d2479c0314a423d
skicodes <skalrashed02@hotmail.com>
2025-09-13 16:33:28 +0000

    aha

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -9,15 +9,14 @@
  */
 int main(int argc, char *argv[])
 {
-    int i;
+	int i;
 
-    (void)argc;
+	(void)argc;
 
-    for (i = 0; argv[0][i] != '\0'; i++)
-        _putchar(argv[0][i]);
+	for (i = 0; argv[0][i] != '\0'; i++)
+		_putchar(argv[0][i]);
 
-    _putchar('\n');
+	_putchar('\n');
 
-    return (0);
+	return (0);
 }
-
