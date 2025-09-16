f60893e90a183d19f0b6af531caa114885a92717
skicodes <skalrashed02@hotmail.com>
2025-09-16 00:19:05 +0000

    djfhdk

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,5 +1,4 @@
 #include <stdlib.h>
-#include <string.h>
 #include "dog.h"
 
 /**
@@ -8,48 +7,56 @@
  * @age: age of the dog
  * @owner: owner of the dog
  *
- * Return: pointer to the new dog, or NULL on failure
+ * Return: pointer to new dog, or NULL if allocation fails
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
-	char *name_copy, *owner_copy;
+	char *name_copy;
+	char *owner_copy;
+	int i;
 
-	/* allocate memory for the dog structure */
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return NULL;
 
-	/* allocate and copy the name */
+	/* Copy name manually */
 	if (name != NULL)
 	{
-		name_copy = malloc(strlen(name) + 1);
+		for (i = 0; name[i]; i++)
+			;
+		name_copy = malloc(i + 1);
 		if (name_copy == NULL)
 		{
 			free(dog);
 			return NULL;
 		}
-		strcpy(name_copy, name);
+		for (i = 0; name[i]; i++)
+			name_copy[i] = name[i];
+		name_copy[i] = '\0';
 	}
 	else
 		name_copy = NULL;
 
-	/* allocate and copy the owner */
+	/* Copy owner manually */
 	if (owner != NULL)
 	{
-		owner_copy = malloc(strlen(owner) + 1);
+		for (i = 0; owner[i]; i++)
+			;
+		owner_copy = malloc(i + 1);
 		if (owner_copy == NULL)
 		{
 			free(name_copy);
 			free(dog);
 			return NULL;
 		}
-		strcpy(owner_copy, owner);
+		for (i = 0; owner[i]; i++)
+			owner_copy[i] = owner[i];
+		owner_copy[i] = '\0';
 	}
 	else
 		owner_copy = NULL;
 
-	/* assign values */
 	dog->name = name_copy;
 	dog->age = age;
 	dog->owner = owner_copy;
