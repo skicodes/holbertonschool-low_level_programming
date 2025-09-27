c808646c85fe2093a5e0ec58228b4b4119593448
skicodes <skalrashed02@hotmail.com>
2025-09-27 16:35:33 +0000

    djdjjd

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,13 +1,13 @@
-#include "function_pointers.h"
 #include <stddef.h>
+#include "function_pointers.h"
 
 /**
  * print_name - Calls a function to print a name
- * @name: The name to be printed
- * @f: Pointer to a function that takes a string and returns nothing
+ * @name: The name to print
+ * @f: Function pointer to a printing function
  */
 void print_name(char *name, void (*f)(char *))
 {
-    if (name != NULL && f != NULL)
-        f(name);
+	if (name != NULL && f != NULL)
+		f(name);
 }
--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,7 +1,12 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
+/**
+ * function_pointers.h - Header file for function pointer utilities
+ * @f: Function pointer to be used in functions
+ */
+
 void print_name(char *name, void (*f)(char *));
-int _putchar(char c); /* if you use _putchar elsewhere */
+int _putchar(char c);
 
 #endif /* FUNCTION_POINTERS_H */
